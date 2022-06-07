af973300ff4fe5a02062fef8a22e80738b20bb17
Bonifacio M. De Robles <deroblesb1@gmail.com>
2022-06-08 04:38:47 +0700

    put some space, clear input when submit, can reuse form

--- contact-tracing-vb/Form1.vb
+++ contact-tracing-vb/Form1.vb
@@ -10,13 +10,13 @@ Public Class Form1
             sw.WriteLine(datenow.ToLongDateString())
             sw.WriteLine(datenow.ToShortTimeString())
 
-            sw.WriteLine(lblFname.Text & " " + txtbxFname.Text)
+            sw.WriteLine(vbCrLf & lblFname.Text & " " + txtbxFname.Text)
             sw.WriteLine(lblLname.Text & " " + txtbxLname.Text)
             sw.WriteLine(lblCnumber.Text & " " + txtbxCnumber.Text)
             sw.WriteLine(lblAge.Text & " " + txtbxAge.Text)
             sw.WriteLine(lblGender.Text & " " + cbxGender.Text)
             sw.WriteLine(lblAddress.Text & " " + txtbxAddress.Text)
-            sw.WriteLine(lblQuestion2.Text)
+            sw.WriteLine(vbCrLf & lblQuestion2.Text)
             sw.WriteLine(lblFever.Text & " " + cboxFever.Text)
             sw.WriteLine(lblCough.Text & " " + cboxCough.Text)
             sw.WriteLine(lblColds.Text & " " + cboxColds.Text)
@@ -25,12 +25,29 @@ Public Class Form1
             sw.WriteLine(lbldiarrhea.Text & " " + cBoxDiarrhea.Text)
 
             If radbuttonYes.Checked = True Then
-                sw.WriteLine(gbxQuestion1.Text + radbuttonYes.Text)
+                sw.WriteLine(vbCrLf & gbxQuestion1.Text + radbuttonYes.Text)
             Else
                 sw.WriteLine(vbCrLf & gbxQuestion1.Text & " " + radbuttonNo.Text)
             End If
 
             sw.Close()
+
+            txtbxFname.Text = ""
+            txtbxLname.Text = ""
+            txtbxCnumber.Text = ""
+            txtbxAge.Text = ""
+            txtbxAddress.Text = ""
+
+            cbxGender.SelectedIndex = -1
+            cboxFever.SelectedIndex = -1
+            cboxCough.SelectedIndex = -1
+            cboxColds.SelectedIndex = -1
+            cboxSorethroat.SelectedIndex = -1
+            cboxDiffInBreath.SelectedIndex = -1
+            cBoxDiarrhea.SelectedIndex = -1
+
+            radbuttonYes.Checked = False
+            radbuttonNo.Checked = False
 
         End Using
     End Sub
